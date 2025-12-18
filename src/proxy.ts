a4638ebf236b1975e4ff4b38e0d59b310f328404
Isaac Stefanello <isaac.stefanello@controlsoft.com.br>
2025-12-18 17:41:42 -0300

    proxy

---
+++ src/proxy.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function proxy(request: NextRequest) {
+  return NextResponse.redirect(new URL('/home', request.url))
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request: NextRequest) { ... }
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/about/:path*',
+}
